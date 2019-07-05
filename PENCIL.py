8a89ed50b6ecd998439b09947b59412dccbca93f
yikun2019 <laneyikun@foxmail.com>
2019-07-05 10:13:35 +0800

    Update PENCIL.py

--- PENCIL.py
+++ PENCIL.py
@@ -486,7 +486,7 @@ def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
     batch_size = target.size(0)
 
-    pred = output.topk(maxk, 1, True, True)
+    _ , pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
 
